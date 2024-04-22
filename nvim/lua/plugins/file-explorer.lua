af7db2be6ea87c0a300ac870a539003dffbba86c
Alberto Silva <albertossilva@gmail.com>
2024-04-22 11:04:06 +0200

    Installing Oil

---
+++ nvim/lua/plugins/file-explorer.lua
@@ -0,0 +1,91 @@
+return {
+  {
+    "kyazdani42/nvim-tree.lua",
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    config = function()
+      local nvim_tree = require("nvim-tree")
+
+      nvim_tree.setup({
+        hijack_cursor = true,
+        update_focused_file = {
+          -- enable = true,
+          -- update_cwd = true,
+        },
+        renderer = {
+          root_folder_modifier = ":t",
+          icons = {
+            glyphs = {
+              default = "",
+              symlink = "",
+              folder = {
+                arrow_open = "",
+                arrow_closed = "",
+                default = "",
+                open = "",
+                empty = "",
+                empty_open = "",
+                symlink = "",
+                symlink_open = "",
+              },
+              git = {
+                unstaged = "",
+                staged = "",
+                unmerged = "",
+                renamed = "➜",
+                untracked = "U",
+                deleted = "",
+                ignored = "◌",
+              },
+            },
+          },
+        },
+        diagnostics = {
+          enable = true,
+          show_on_dirs = true,
+          icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+          },
+        },
+        view = {
+          adaptive_size = true,
+          width = 50,
+          side = "left",
+        },
+      })
+    end,
+  },
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    config = function()
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+      vim.keymap.set("n", "<leader>f", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+      require("oil").setup({
+        default_file_explorer = true,
+        keymaps = {
+          ["g?"] = "actions.show_help",
+          ["<CR>"] = "actions.select",
+          ["<C-v>"] = "actions.select_vsplit",
+          ["<C-x>"] = "actions.select_split",
+          ["<C-t>"] = "actions.select_tab",
+          ["<C-p>"] = "actions.preview",
+          ["<C-c>"] = "actions.close",
+          ["<C-l>"] = "actions.refresh",
+          ["-"] = "actions.parent",
+          ["_"] = "actions.open_cwd",
+          ["`"] = "actions.cd",
+          ["~"] = "actions.tcd",
+          ["gs"] = "actions.change_sort",
+          ["gx"] = "actions.open_external",
+          ["g."] = "actions.toggle_hidden",
+          ["g\\"] = "actions.toggle_trash",
+        },
+      })
+    end,
+  },
+}
