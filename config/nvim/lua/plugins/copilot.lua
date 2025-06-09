14ac9905bb3f5c5ed3c64ffd9ac49a5579051689
Alberto Silva <albertossilva@gmail.com>
2025-06-09 15:00:16 +0200

    Dont force tiktoken to install every time

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -5,14 +5,10 @@ return {
       {
         "vhyrro/luarocks.nvim",
         priority = 1000,
-        opts = {
-          rocks = { "tiktoken_core" },
-        },
       },
       { "github/copilot.vim" },
       { "nvim-lua/plenary.nvim", branch = "master" },
     },
-    build = "make tiktoken",
     opts = {},
   },
 }
